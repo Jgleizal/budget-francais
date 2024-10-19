df7baed4d4aa2313b7780f7dbdbc2181190236cf
Jgleizal <jeremygleizal@gmail.com>
2024-10-20 00:33:59 +0200

    Add 'test_data_preparation.py', 'data_preparation.py', 'eda.py'& parquet data file

--- src/eda.py
+++ src/eda.py
@@ -18,14 +18,33 @@ def eda_overview(df):
 
 def visualize_budget_distribution(df):
     """
-    Visualise la répartition du budget par ministère et année.
-    :param df: DataFrame Pandas contenant les colonnes 'année', 'ministère', 'dépenses'.
+    Visualise la répartition du budget par ministère et année, triée du plus petit au plus grand.
+    :param df: DataFrame Pandas contenant les colonnes 'annee', 'balance_sortie', 'libelle_ministere'.
     """
-    plt.figure(figsize=(10, 6))
-    sns.barplot(x='année', y='dépenses', data=df, hue='ministère')
-    plt.title('Répartition des dépenses par ministère et par année')
+    # Convertir la colonne 'annee' en numérique au cas où elle serait mal formatée
+    df['annee'] = pd.to_numeric(df['annee'], errors='coerce')
+
+    # Trier les données par année
+    df_sorted = df.sort_values(by='annee', ascending=True)
+
+    # Créer le barplot avec les années triées
+    plt.figure(figsize=(12, 6))
+    sns.barplot(x='annee', y='balance_sortie', data=df_sorted, hue='libelle_ministere')
+
+    # Ajuster la taille de la légende pour les ministères
+    plt.title('Répartition des dépenses par ministère et par année (triée)')
     plt.xticks(rotation=45)
+
+    # Placer la légende en dehors du graphique
+    plt.legend(title='Ministère', fontsize=8, title_fontsize='10', bbox_to_anchor=(1.05, 1), loc='upper left',
+               borderaxespad=0.)
+
+    # Ajuster les marges pour que la légende soit bien visible
+    plt.tight_layout()
+
+    # Afficher le graphique
     plt.show()
+
 
 
 # Exemple d'utilisation
